34876814f688bc091cdd0c1608bd514b69787b19
Nanaya <nanayamiki@gmail.com>
2024-12-16 17:14:16 +0900

    CRUD with array

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -6,21 +6,49 @@ string petFish = "pupper fish";
 
 petFish = "porcupine " + petFish;
 
-Console.WriteLine($"I AM BUYING {petFish}");
-Console.WriteLine($"yes, ${petFish}");
+// Console.WriteLine($"I AM BUYING {petFish}");
+// Console.WriteLine($"yes, ${petFish}");
 
-Console.WriteLine(petFish);
+// Console.WriteLine(petFish);
 
 //UPDATE
 //petfish[0] = "P" // does not work with C#
 string newPetFish = petFish.Replace("porcupine", "blue dot");
 
-Console.WriteLine(newPetFish);
+//Console.WriteLine(newPetFish);
 
 //DELETE
 //String Builder
 StringBuilder newPet = new ();
 newPet.Append("shiba inu");
-Console.WriteLine(newPet);
+//Console.WriteLine(newPet);
 newPet.Remove(0, 5);
-Console.WriteLine(newPet);
+//Console.WriteLine(newPet);
+
+//Array CRUD
+
+//string
+string[] favoriteDogs = ["shiba", "beagle", "husky", "pug"];
+
+//Read
+//Console.WriteLine(favoriteDogs);  wrong, you need to loop
+
+foreach(var dog in favoriteDogs)
+{
+	//Console.WriteLine(dog);
+}
+
+//Update
+favoriteDogs[0] = "chiwawa";
+foreach(var dog in favoriteDogs)
+{
+	//Console.WriteLine(dog);
+}
+
+//Update with LINQ
+var newFavoriteDogs = favoriteDogs.Where((e) => e.StartsWith('b'));
+
+foreach(var dog in newFavoriteDogs)
+{
+	Console.WriteLine(dog);
+}
