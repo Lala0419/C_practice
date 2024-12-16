59f2d040412b83dbcbdfe7bb1d88df777a70279b
Nanaya <nanayamiki@gmail.com>
2024-12-16 15:33:39 +0900

    add consoleApp

---
+++ consoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information;
+
+string billGates = "Praise the great leader... Bill Gates";
+
+Console.WriteLine(billGates);
+Console.WriteLine(billGates);
