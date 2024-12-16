06d1a36f217be61543980cb2d4042ced4d8caedd
Nanaya <nanayamiki@gmail.com>
2024-12-16 16:41:31 +0900

    upating variable

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -8,3 +8,9 @@ Console.WriteLine($"I AM BUYING {petFish}");
 Console.WriteLine($"yes, ${petFish}");
 
 Console.WriteLine(petFish);
+
+//UPDATE
+//petfish[0] = "P" // does not work with C#
+string newPetFish = petFish.Replace("porcupine", "blue dot");
+
+Console.WriteLine(newPetFish);
